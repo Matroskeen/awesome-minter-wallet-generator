d2330cd91c0fd08df329e20404a87d2097e08cc3
Ivan Doroshenko <ivan.drshnk@gmail.com>
2020-01-05 19:00:55 +0200

    Fixed counter behavior.

--- js/main.js
+++ js/main.js
@@ -15,13 +15,12 @@ function start() {
   }
 
   var inEl = document.getElementById("in");
-  var counterEl = document.getElementById("counter");
   var attemptsEl = document.getElementById("attempts");
 
   // Let's go back to initial state after refresh (stop-start).
   display([attemptsEl], "block");
   mnemonicInfoHide();
-  counterEl.innerHTML = "0";
+  initCounter();
   generationInfoShow();
 
   if (typeof(worker) == "undefined") {
@@ -34,8 +33,8 @@ function start() {
   worker.onmessage = function(event) {
     generatedWallet = event.data;
 
-    generatedAddressShow(generatedWallet);
-    counterEl.innerHTML = parseInt(counterEl.innerHTML) + 1;
+    generatedAddressShow();
+    updateCounter();
 
     if (generatedWallet.match) {
       stop();
@@ -92,9 +91,25 @@ function generationInfoShow() {
   display(["generation-info", "generation-divider"], "block");
 }
 
-function generatedAddressShow(wallet, input) {
-  var reg = new RegExp(wallet.input, 'gi');
-  document.getElementById("out").innerHTML = wallet.address.replace(reg, function(str) {return '<span class="match">' + str + '</span>'});
+function generatedAddressShow() {
+  var reg = new RegExp(generatedWallet.input, 'gi');
+  document.getElementById("out").innerHTML = generatedWallet.address.replace(reg, function(str) {return '<span class="match">' + str + '</span>'});
+}
+
+function initCounter() {
+  if (typeof generatedWallet !== "undefined" && generatedWallet.input !== "") {
+    var currentIn = document.getElementById("in").value;
+
+    // We'll clear counter only if user input was changed.
+    if (currentIn !== generatedWallet.input) {
+      document.getElementById("counter").innerHTML = "0";
+    }
+  }
+}
+
+function updateCounter() {
+  var counterEl = document.getElementById("counter");
+  counterEl.innerHTML = parseInt(counterEl.innerHTML) + 1;
 }
 
 function validateInput() {
